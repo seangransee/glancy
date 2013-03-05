5fa265e2261be636b713cdb8f779836bc7487317
Sean Gransee <sean.gransee@gmail.com>
2013-03-05 15:52:45 -0600

    get size from URL

--- app.rb
+++ app.rb
@@ -2,7 +2,11 @@ require 'sinatra'
 require 'coffee-script'
 require 'sass'
 require 'boilerpipe'
+require 'json'
 
 get '/' do
-  haml :index
+  url = params['url']
+  articletext = Boilerpipe.extract(url, {:output => :text, :extractor => :ArticleExtractor})
+  size = articletext.split.size
+  {:url => url, :size => size}.to_json
 end
