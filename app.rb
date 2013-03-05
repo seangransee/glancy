a9cc8aa37a1bc74bb2d358218b97f90e09ecbc51
Sean Gransee <sean.gransee@gmail.com>
2013-03-05 17:41:16 -0600

    allow hacker news

--- app.rb
+++ app.rb
@@ -2,7 +2,12 @@ require 'sinatra'
 require 'boilerpipe'
 require 'json'
 
+configure do
+  enable :cross_origin
+end
+
 get '/' do
+  headers['Access-Control-Allow-Origin'] = ['http://news.ycombinator.com']
   url = params['url']
   if url
     articletext = Boilerpipe.extract(url, {:output => :text, :extractor => :ArticleExtractor})
