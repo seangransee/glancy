1ba9750be8369b94967f3bd9cd4662fa638af694
Sean Gransee <sean.gransee@gmail.com>
2013-03-05 15:56:05 -0600

    empty URL case

--- app.rb
+++ app.rb
@@ -6,7 +6,11 @@ require 'json'
 
 get '/' do
   url = params['url']
-  articletext = Boilerpipe.extract(url, {:output => :text, :extractor => :ArticleExtractor})
-  size = articletext.split.size
-  {:url => url, :size => size}.to_json
+  if url
+    articletext = Boilerpipe.extract(url, {:output => :text, :extractor => :ArticleExtractor})
+    size = articletext.split.size
+    {:url => url, :size => size}.to_json
+  else
+    "No URL passed in"
+  end
 end
