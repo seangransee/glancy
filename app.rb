253333eeba64a071f1ca7ddd70ab0383e9c390bc
Sean Gransee <sean.gransee@gmail.com>
2013-03-05 19:14:46 -0600

    use database

--- app.rb
+++ app.rb
@@ -1,18 +1,56 @@
 require 'sinatra'
 require 'boilerpipe'
 require 'json'
+require 'mongo'
+
+include Mongo
+
+server = ENV['ALG_MONGOSERVER']
+port = ENV['ALG_MONGOPORT']
+user = ENV['ALG_MONGOUSER']
+pass = ENV['ALG_MONGOPASS']
+
+client = MongoClient.new(server, port)
+db = client['article-length-glance']
+db.authenticate(user, pass)
+$coll = db['sizes']
 
 configure do
   enable :cross_origin
+end
+
+def lookup(url)
+  site = $coll.find_one("url" => url)
+  if site
+    size = site["size"]
+    {:url => url, :size => size}.to_json
+  else
+    nil
+  end
+end
+
+def addToDatabase(url, size)
+  doc = {"url" => url, "size" => size, "time" => Time.now}
+  $coll.insert(doc)
+end
+
+def getSize(url)
+  from_database = lookup(url)
+  if from_database
+    from_database
+  else
+    articletext = Boilerpipe.extract(url, {:output => :text, :extractor => :ArticleExtractor})
+    size = articletext.split.size
+    addToDatabase(url, size)
+    {:url => url, :size => size}.to_json
+  end
 end
 
 get '/' do
   headers['Access-Control-Allow-Origin'] = ['http://news.ycombinator.com']
   url = params['url']
   if url
-    articletext = Boilerpipe.extract(url, {:output => :text, :extractor => :ArticleExtractor})
-    size = articletext.split.size
-    {:url => url, :size => size}.to_json
+    getSize(url)
   else
     "No URL passed in"
   end
