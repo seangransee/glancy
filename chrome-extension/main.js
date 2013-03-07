90788d33638ae3758d59ad5a9512db5fbf25cee7
Sean Gransee <sean.gransee@gmail.com>
2013-03-07 12:04:59 -0600

    refactoring

--- chrome-extension/main.js
+++ chrome-extension/main.js
@@ -16,38 +16,52 @@ function createSizeDiv(site, color, size) {
   return $(document.createElement('div')).addClass(site).addClass('size').addClass(color).width(size);
 }
 
+function placeBar(link, sizeDiv, site) {
+  switch (site)
+  {
+    case 'hn':
+      $(link).parent().parent().before(sizeDiv);
+      break;
+    case 'reddit':
+      $(link).parent().parent().parent().before(sizeDiv);
+      break;
+  }
+}
+
+function generateBars(maxWidth, isPageOfLinks, links, scale, site) {
+  if(isPageOfLinks) {
+    $(links).each(function() {
+      var url = $(this).prop('href');
+      var link = this;
+      getSize(url, function(size) {
+        barSize = size / scale;
+        if(barSize > maxWidth) {
+          barSize = maxWidth;
+        }
+        var sizeDiv = createSizeDiv(site, color, barSize);
+        placeBar(link, sizeDiv, site);
+      });
+    });
+  }
+}
+
 switch (window.location.origin)
 {
   case 'http://news.ycombinator.com':
     var maxWidth = $('table').first().find('tbody').first().width() - 36;
-    $('.title a').each(function() {
-      var url = $(this).prop('href');
-      var link = this;
-      getSize(url, function(size) {
-        barSize = size / 5;
-        if(barSize > maxWidth) {
-          barSize = maxWidth;
-        }
-        var sizeDiv = createSizeDiv('hn', color, barSize);
-        $(link).parent().parent().before(sizeDiv);
-      });
-    });
+    var isPageOfLinks = $('.title a').length > 1;
+    var links = $('.title a');
+    var scale = 5;
+    var site = 'hn';
     break;
 
   case 'http://www.reddit.com':
     var maxWidth = $(window).width() - 70;
-    $('.linklisting a.title').each(function() {
-      var url = $(this).prop('href');
-      var link = this;
-      getSize(url, function(size) {
-        barSize = size / 5;
-        if(barSize > maxWidth) {
-          barSize = maxWidth;
-        }
-        var sizeDiv = createSizeDiv('reddit', color, barSize);
-        $(link).parent().parent().parent().before(sizeDiv);
-      });
-    });
+    var isPageOfLinks = true;
+    var links = $('.linklisting a.title');
+    var scale = 5;
+    var site = 'reddit';
 }
 
+generateBars(maxWidth, isPageOfLinks, links, scale, site);
 chrome.extension.sendRequest({method: "changeIconColor", color: color});
